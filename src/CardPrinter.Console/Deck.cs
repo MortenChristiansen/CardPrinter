f3bdd9288df4e484694f257d2f98b0f7e3fd5296
Morten Christiansen <mc@uvdata.dk>
2017-11-21 22:26:31 +0100

    Support for parsing and validating deck specification files

---
+++ src/CardPrinter.Console/Deck.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardPrinter.Console
+{
+    class Deck
+    {
+        public string Name { get; set; }
+        public string RootPath { get; set; }
+        public Card[] Cards { get; set; }
+
+        public class Card
+        {
+            public string FileName { get; set; }
+            public int Count { get; set; }
+        }
+    }
+}
