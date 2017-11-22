3f38ca6cee572152cfc42fd01e71c9832eda3716
Morten Christiansen <mc@uvdata.dk>
2017-11-22 12:46:50 +0100

    All images are added to pdf

--- src/CardPrinter.Console/PdfFormatter.cs
+++ src/CardPrinter.Console/PdfFormatter.cs
@@ -8,28 +8,60 @@ namespace CardPrinter.Console
     static class PdfFormatter
     {
         private static readonly float PixelsPerMillimeter = PageSize.A4.Height / 297;
+        private static readonly float Margin = 0 * PixelsPerMillimeter;
 
         public static void Format(Deck deck, DeckInfo deckInfo)
         {
-            var image1 = deckInfo.CardCounts.First();
             var pdfName = $"output/{deck.Name}.pdf";
 
             if (File.Exists(pdfName))
                 File.Delete(pdfName);
 
-            var margin = 5 * PixelsPerMillimeter;
-            var pdfDoc = new Document(PageSize.A4, margin, margin, margin, margin);
+            var pdfDoc = new Document(PageSize.A4, Margin, Margin, Margin, Margin);
             var writer = PdfWriter.GetInstance(pdfDoc, new FileStream(pdfName, FileMode.OpenOrCreate));
 
             pdfDoc.Open();
             var content = writer.DirectContent;
-            var path = Path.GetFullPath(image1.imagePath);
-            var image = Image.GetInstance(path);
-            image.SetAbsolutePosition(margin, margin);
-            image.ScaleAbsolute((float)deck.CardWidth * PixelsPerMillimeter, (float)deck.CardHeight * PixelsPerMillimeter);
-            pdfDoc.Add(image);
+
+            var pageCardIndex = 0;
+            foreach (var card in deckInfo.CardCounts)
+            {
+                for (int i = 0; i < card.count; i++)
+                {
+                    AddImage(deck.CardWidth, deck.CardHeight, card.imagePath, pdfDoc, ref pageCardIndex);
+                }
+            }
 
             pdfDoc.Close();
+        }
+
+        private static void AddImage(decimal cardWidthInMm, decimal cardHeightInMm, string imagePath, Document pdfDoc, ref int pageCardIndex)
+        {
+            var availableWidth = PageSize.A4.Width - (2 * Margin);
+            var availableHeight = PageSize.A4.Height - (2 * Margin);
+            var cardWidth = (float)cardWidthInMm * PixelsPerMillimeter;
+            var cardHeight = (float)cardHeightInMm * PixelsPerMillimeter;
+            var cardsPerRow = (int)(availableWidth / cardWidth);
+            var cardsPerColumn = (int)(availableHeight / cardHeight);
+
+            if (pageCardIndex >= cardsPerRow * cardsPerColumn)
+            {
+                pageCardIndex = 0;
+                pdfDoc.NewPage();
+            }
+
+            var x = pageCardIndex % cardsPerRow;
+            var y = pageCardIndex / cardsPerRow;
+
+            var positionX = Margin + (x * cardWidth);
+            var positionY = PageSize.A4.Height - (Margin + (y * cardHeight)) - cardHeight;
+
+            var path = Path.GetFullPath(imagePath);
+            var image = Image.GetInstance(path);
+            image.SetAbsolutePosition(positionX, positionY);
+            image.ScaleAbsolute(cardWidth, cardHeight);
+            pdfDoc.Add(image);
+            pageCardIndex++;
         }
     }
 }
